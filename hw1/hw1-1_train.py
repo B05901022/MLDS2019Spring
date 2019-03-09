fa3dc8bfd86b3327dc6f92cab81f0672101cb8c5
B05901022 <b05901022@ntu.edu.tw>
2019-03-09 16:11:17 +0800

    hw1

--- hw1/hw1-1_train.py
+++ hw1/hw1-1_train.py
@@ -29,29 +29,29 @@ class shallow(nn.Module):
     def __init__(self):
         super(shallow, self).__init__()
         self.conv_layer = nn.Sequential(
-                nn.Conv2d(3, 16, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
-                nn.BatchNorm2d(16),
+                nn.Conv2d(3, 8, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
+                nn.BatchNorm2d(8),
                 nn.LeakyReLU(),
-                nn.Conv2d(16, 16, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
-                nn.BatchNorm2d(16),
+                nn.Conv2d(8, 8, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
+                nn.BatchNorm2d(8),
                 nn.LeakyReLU(),
                 nn.AvgPool2d((2,2)), #16 * 16
-                nn.Conv2d(16, 32, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
-                nn.BatchNorm2d(32),
+                nn.Conv2d(8, 16, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
+                nn.BatchNorm2d(16),
                 nn.LeakyReLU(),
-                nn.Conv2d(32, 32, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
-                nn.BatchNorm2d(32),
+                nn.Conv2d(16, 16, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
+                nn.BatchNorm2d(16),
                 nn.LeakyReLU(),
                 nn.AvgPool2d((2,2)) #8 * 8
                 )
         self.dnn_layer = nn.Sequential(
-                nn.Linear(32*8*8, 128),
-                nn.BatchNorm1d(128),
+                nn.Linear(16*8*8, 64),
+                nn.BatchNorm1d(64),
                 nn.LeakyReLU(),
-                nn.Linear(128, 128),
-                nn.BatchNorm1d(128),
+                nn.Linear(64, 64),
+                nn.BatchNorm1d(64),
                 nn.LeakyReLU(),
-                nn.Linear(128, 10))
+                nn.Linear(64, 10))
         
     def forward(self, x):
         x = self.conv_layer(x)
