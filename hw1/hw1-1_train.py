54da6eb44599bc5e23fef675eba18e9d62fe3b31
B05901022 <b05901022@ntu.edu.tw>
2019-03-09 17:14:48 +0800

    hw1

--- hw1/hw1-1_train.py
+++ hw1/hw1-1_train.py
@@ -111,9 +111,9 @@ def main():
             
         torch.save(train_model, 'shallow_model.pkl')
         torch.save(optimizer.state_dict(), 'shallow_model.optim')
-        print("")
+        print("")   
         print("Epoch loss: ", epoch_loss / BATCHSIZE)
-        print("Epoch acc:  ", epoch_acc  / BATCHSIZE)       
+        print("Epoch acc:  ", epoch_acc  / len(train_data))       
         
     return 
     
