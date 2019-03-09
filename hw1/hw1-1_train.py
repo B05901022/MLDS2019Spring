67e5baa1df2c0900dc4e25d0076078978af9cba0
B05901022 <b05901022@ntu.edu.tw>
2019-03-09 15:58:08 +0800

    hw1

--- hw1/hw1-1_train.py
+++ hw1/hw1-1_train.py
@@ -29,23 +29,23 @@ class shallow(nn.Module):
     def __init__(self):
         super(shallow, self).__init__()
         self.conv_layer = nn.Sequential(
-                nn.Conv2d(3, 32, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
-                nn.BatchNorm2d(32),
+                nn.Conv2d(3, 16, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
+                nn.BatchNorm2d(16),
                 nn.LeakyReLU(),
-                nn.Conv2d(32, 32, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
-                nn.BatchNorm2d(32),
+                nn.Conv2d(16, 16, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
+                nn.BatchNorm2d(16),
                 nn.LeakyReLU(),
                 nn.AvgPool2d((2,2)), #16 * 16
-                nn.Conv2d(32, 64, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
-                nn.BatchNorm2d(64),
+                nn.Conv2d(16, 32, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
+                nn.BatchNorm2d(32),
                 nn.LeakyReLU(),
-                nn.Conv2d(64, 64, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
-                nn.BatchNorm2d(64),
+                nn.Conv2d(32, 32, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
+                nn.BatchNorm2d(32),
                 nn.LeakyReLU(),
                 nn.AvgPool2d((2,2)) #8 * 8
                 )
         self.dnn_layer = nn.Sequential(
-                nn.Linear(64*8*8, 128),
+                nn.Linear(32*8*8, 128),
                 nn.BatchNorm1d(128),
                 nn.LeakyReLU(),
                 nn.Linear(128, 128),
