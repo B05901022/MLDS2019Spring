962ad11560688c8bf61b7ec96884edca3f63444b
Serenade34 <43457610+Serenade34@users.noreply.github.com>
2019-03-11 21:15:22 +0800

    Update hw1-2_task1_plot.py

--- hw1/hw1-2/hw1-2_task1_plot.py
+++ hw1/hw1-2/hw1-2_task1_plot.py
@@ -15,6 +15,31 @@ import argparse
 import sklearn.decomposition
 a=[]
 b=[]
+class net_shallow(nn.Module):
+    def __init__(self):
+        super(net_shallow, self).__init__()
+        self.conv_layer = nn.Sequential(
+                nn.Conv2d(3, 8, kernel_size=(3,3), stride=(1,1), padding=1), #32 * 32
+                nn.BatchNorm2d(8),
+                nn.ReLU(),
+                nn.AvgPool2d((2,2)), #16 * 16
+                nn.Conv2d(8, 16, kernel_size=(3,3), stride=(1,1), padding=1), #16 * 16
+                nn.BatchNorm2d(16),
+                nn.ReLU(),
+                nn.AvgPool2d((2,2)) #8 * 8
+                )
+        self.dnn_layer = nn.Sequential(
+                nn.Linear(784, 64),
+                nn.ReLU(),
+                nn.Linear(64, 64),
+                nn.ReLU(),
+                nn.Linear(64, 10))
+        
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        output = self.dnn_layer(x)
+        return output
+
 pca=sklearn.decomposition.PCA(2)
 for k in range(1,9,1):
     for i in range(10):
