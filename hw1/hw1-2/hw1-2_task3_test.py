d538eced7e1c027b0ba336136b550b24af1e3188
b05901022 <b05901022@ntu.edu.tw>
2019-03-16 00:33:11 +0800

    hw1

--- hw1/hw1-2/hw1-2_task3_test.py
+++ hw1/hw1-2/hw1-2_task3_test.py
@@ -23,7 +23,7 @@ torch.manual_seed(1)
 ###HYPERPARAMETER###
 EPOCH = 10
 BATCHSIZE = 500
-ADAMPARAM = {'lr':0.001, 'betas':(0.9, 0.999), 'eps':1e-08}
+ADAMPARAM = {'lr':0.00001, 'betas':(0.9, 0.999), 'eps':1e-08}
 
 ###MODEL###
 class net(nn.Module):
@@ -78,26 +78,31 @@ def get_second_order_grad(xs, ys):
     return grads2
 
 def get_gradient_norm(params):
-    grad_all = 0.0
+    grad_all = torch.Tensor([0])
+    
     #total_norm = 0.0
     for p in params:
-        grad = 0.0
+        grad = torch.Tensor([0])
+        
         if p.grad is not None:
-            grad = p.grad.data.norm(2)
+            grad += torch.norm(p.grad)**2#p.grad.data.norm(2)
+        grad.requires_grad = True
         grad_all += grad
-    return grad_all ** 0.5
+    #grad_all.requires_grad = True
+    #print(grad_all)
+    return (grad_all ** 0.5).to(device)
 
 def criterion(pred, true, params):
     org_loss = nn.MSELoss()
     loss = org_loss(pred, true)
-    loss += get_gradient_norm(params)
+    loss += get_gradient_norm(params)[0]
     return loss
 
 #===============================================================================================
 
 def main(args):
     ###Load dataset###
-    train_x = np.random.rand(50000).reshape(50000,1)
+    train_x = np.random.rand(500000).reshape(500000,1)
     train_y = torch.from_numpy(exp_func(train_x))
     train_x = torch.from_numpy(train_x)
     train_data = Data.TensorDataset(train_x, train_y) 
@@ -114,7 +119,7 @@ def main(args):
     
     ###OPTIMIZER###
     #optimizer = torch.optim.Adam(train_model.parameters(), lr=ADAMPARAM['lr'], betas=ADAMPARAM['betas'], eps=ADAMPARAM['eps'], weight_decay=1e-5)
-    optimizer = torch.optim.Adagrad(train_model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adagrad(train_model.parameters(), lr=0.0001)
     
     ###LOSS FUNCTION###
     loss_func = nn.MSELoss()
@@ -122,9 +127,9 @@ def main(args):
     ###LOSS RECORD###
     loss_history = []
     
-    """
+    
     ###Training###    
-    for e in range(EPOCH):
+    for e in range(EPOCH*0):
         
         #print("Epoch ", e)
         epoch_loss = 0
@@ -144,18 +149,22 @@ def main(args):
             print("Epoch", e, "Batch: ", b_num, "loss: ", loss.item(), end = '\r')
             epoch_loss += loss.item()
             
-        torch.save(train_model, 'hw1-2_task3_step1_function_model_'+str(e)+'.pkl')
-        torch.save(optimizer.state_dict(), 'hw1-2_task3_step1_function_model_'+str(e)+'.optim')
+        #torch.save(train_model, 'hw1-2_task3_step1_function_model_'+str(e)+'.pkl')
+        #torch.save(optimizer.state_dict(), 'hw1-2_task3_step1_function_model_'+str(e)+'.optim')
         #if e%100 == 0:
         print("")
-        print("Epoch loss: ", epoch_loss / len(train_data))
-        loss_history.append( epoch_loss / len(train_data))
-    """
+        print("Epoch loss: ", epoch_loss / len(train_data) * BATCHSIZE)
+        loss_history.append( epoch_loss / len(train_data) * BATCHSIZE)
+    
     
     ###SECOND TRAINING###
     
+    print('Second part...')
+    
     ratio_list = []
     loss_list  = []
+    ratio_list_2 = []
+    loss_list_2 = []
     
     for e in range(EPOCH):
         
@@ -170,7 +179,10 @@ def main(args):
             b_y = b_y.float()
             optimizer.zero_grad()
             pred = train_model(b_x)
-            loss = criterion(pred, b_y, optimizer.param_groups[0]['params'])
+            loss = loss_func(pred, b_y)
+            loss.backward()
+            loss_new = criterion(pred, b_y, optimizer.param_groups[0]['params'])
+            """
             Hessian = np.array(get_second_order_grad(optimizer.param_groups[0]['params'], loss))
             #w_t = np.array(optimizer.param_groups[0]['params'])
             minimas = [np.linalg.eig(i.cpu())[0] for i in Hessian]
@@ -180,29 +192,38 @@ def main(args):
                 tot_len += len(i)
                 pos_eig += np.sum(np.array([1 if j > 0 else 0 for j in i]))
             temp_ratio_list.append(pos_eig/tot_len)
-            loss.backward()
+            """
+            loss_new.backward()
             optimizer.step()
             print("Epoch", e, "Batch: ", b_num, "loss: ", loss.item(), end = '\r')
             epoch_loss += loss.item()
-        
+        """
         ratio_list.append(np.mean(np.array(temp_ratio_list)))
-        loss_list.append(epoch_loss/len(train_data))
-            
+        loss_list.append(epoch_loss/len(train_data) * BATCHSIZE)
+        ratio_list_2 += temp_ratio_list
+        loss_list_2 += [epoch_loss/len(train_data) * BATCHSIZE for i in range(len(temp_ratio_list))]
+        """ 
         torch.save(train_model, 'hw1-2_task3_step2_function_model.pkl')
         torch.save(optimizer.state_dict(), 'hw1-2_task3_step2_function_model.optim')
-        #if e%100 == 0:
+        
         print("")
-        print("Epoch loss: ", epoch_loss / len(train_data))
-        loss_history.append( epoch_loss / len(train_data))
-    
+        print("Epoch loss: ", epoch_loss / len(train_data) * BATCHSIZE)
+        #loss_history.append( epoch_loss / len(train_data) * BATCHSIZE)
+    """
     np.save('loss', np.array(loss_list))
     np.save('minimal_ratio', np.array(ratio_list))
-
+    
+    plt.figure(1)
     plt.scatter(ratio_list, loss_list)
     #plt.ylim(0,0.002)
     plt.savefig('task3.png', format='png')
+    
+    plt.figure(2)
+    plt.scatter(ratio_list_2, loss_list_2)
+    plt.savefig('task3_2.png', format='png')
+    
     plt.show()
-        
+    """   
         
     return 
     
