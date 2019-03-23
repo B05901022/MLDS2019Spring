5b9174ed9dfa717f7abc59919d34b02b7a6b74e5
B05901022 <b05901022@ntu.edu.tw>
2019-03-23 22:57:33 +0800

    hw1

--- hw2/hw1-3_bonus.py
+++ hw2/hw1-3_bonus.py
@@ -97,7 +97,7 @@ def main(args):
         y_test = tf.convert_to_tensor(y_test)
         y_pred = tf.convert_to_tensor(y_pred)
         
-        model_weights = tf.concat([tf.reshape(model.trainable_variables, [-1])])
+        model_weights = tf.concat([tf.reshape(i, [-1]) for i in model.trainable_variables])
         print(len(model_weights))
             
         print("""
