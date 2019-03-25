c5c97b72f63cdefdc103dc6d3e70eeeccf485acd
B05901022 <b05901022@ntu.edu.tw>
2019-03-25 19:17:35 +0800

    hw1

---
+++ hw2/hw1-3_bonus_plot_acc.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 19:15:26 2019
+
+@author: Austin Hsu
+"""
+
+import seaborn as sns
+import pandas as pd
+
+results = pd.read_csv(open('sharpness.csv', 'r'))
+
+results_1 = results[:8]
+results_2 = results[8:]
+
+results_plot = results_2
+
+plot_1 = sns.pointplot("batch_size", "train_acc", data = results_plot, color='blue', logx=True)
+
+plot_2 = sns.pointplot("batch_size", "test_acc", data = results_plot, color='blue', logx=True, linestyles='--')
+
+plot_1.set(xlabel='batch_size(log scale)', ylabel='acc')
+
+plot_2.set(xlabel='batch_size(log scale)', ylabel='acc')
+
+plott = plot_1.twinx()
+
+plot_3 = sns.pointplot("batch_size", "sharpness", data = results_plot, color='red', logx=True)
+
+plot_3.set(xlabel='batch_size(log scale)', ylabel='sharpness')
+
+#sns.pointplot("batch_size", "sharpness", data = results_2, logx=True)
