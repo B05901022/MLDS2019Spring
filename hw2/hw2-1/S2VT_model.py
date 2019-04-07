847442205374a0accd249e5ad9b7bd21d76d0759
b05901022 <b05901022@ntu.edu.tw>
2019-04-07 23:00:42 +0800

    hw2

--- hw2/hw2-1/S2VT_model.py
+++ hw2/hw2-1/S2VT_model.py
@@ -82,15 +82,18 @@ class S2VT(nn.Module):
                 word=self.embedding_layer_o(decoded_output).squeeze(0)
                 sentence.append(word)    
             else:
+                print("correct_answe:", end='')
+                print(correct_answer.shape)
                 correct_answer=torch.squeeze(correct_answer, 2)
                 correct_answer=correct_answer.reshape((44,4,2420))
                 correct_answer=torch.unsqueeze(correct_answer, 1)
+                print("correct_answe:", end='')
+                print(correct_answer.shape)
                 a=correct_answer[s] #.
                 sample=self.embedding_layer_i(a)
                 correct=(encoded_padding[s]).unsqueeze(0)
                 """
-                print("correct_answe:", end='')
-                print(correct_answer.shape)
+                
                 print("a:", end='')
                 print(a.shape)
                 print("sample:", end='')
