d29d811277c7040731823272eae5e043b8129600
b05901022 <b05901022@ntu.edu.tw>
2019-05-11 22:07:21 +0800

    hw3

--- hw3/hw3-1/Model.py
+++ hw3/hw3-1/Model.py
@@ -127,7 +127,7 @@ def main(args):
              transforms.ToTensor(),
              transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
     traindata = torchvision.datasets.ImageFolder(root=args.data_directory, transform=transform)
-    train_dataloader = Data.dataloader(traindata, batchsize=BATCHSIZE)
+    train_dataloader = Data.DataLoader(traindata, batch_size=BATCHSIZE)
     
     """
     //------
@@ -198,9 +198,9 @@ def main(args):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_directory', '-dd', type=str, default='../../../../MLDS_dataset/hw3-1/AnimeDataset/faces/')
+    parser.add_argument('--data_directory', '-dd', type=str, default='../../../MLDS_dataset/hw3-1/AnimeDataset/')
     parser.add_argument('--model_name', '-mn', type=str, default='GAN_1')
-    parser.add_argument('--model_directory', '-md', type=str, default='../../../../MLDS_models/hw3-1/')
+    parser.add_argument('--model_directory', '-md', type=str, default='../../../MLDS_models/hw3-1/')
     parser.add_argument('--epoch', '-e', type=int, default=50)
     parser.add_argument('--k', '-k', type=int, default=3)
     args = parser.parse_args()
