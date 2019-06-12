f38794bc5fb1fb04e4dd964e5fc6db834ac5605f
Serenade34 <43457610+Serenade34@users.noreply.github.com>
2019-06-12 20:23:48 +0800

    Add files via upload

--- hw4/agent_dir/agent_dqn.py
+++ hw4/agent_dir/agent_dqn.py
@@ -20,6 +20,29 @@ class Replay_Memory(object):
     def sample(self):
         return self.memory[np.random.randint(0,self.capacity)]
 """
+np.random.seed(1024)
+torch.cuda.manual_seed(1024)
+torch.manual_seed(1024)
+device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+class DQN_Model(nn.Module):
+    def __init__(self):
+        super (DQN,self).__init__()
+        self.conv1=nn.Conv2d(1,3,kernel_size=4,stride=2,padding=0)
+        self.conv2=nn.Conv2d(3,3,kernel_size=4,stride=2,padding=0)
+        self.flatten=nn.Linear(3*18*18,128)
+        self.dense1=nn.Linear(80*80,128)
+        self.dense2=nn.Linear(256,1)
+    def forward(self,x):
+        x=self.conv1(x)
+        x=nn.ReLU(x)
+        x=self.conv2(x)
+        x=self.flatten(x)
+        x=nn.ReLU(x)
+        x=self.dense1(x)
+        x=nn.ReLU(x)
+        x=self.dense2(x)
+        x=nn.Sigmoid(x)
+        return (x)
 class Agent_DQN(Agent):
     def __init__(self, env, args):
         """
@@ -32,7 +55,7 @@ class Agent_DQN(Agent):
 
         if args.test_dqn:
             print("Loading trained model...")
-            print("Model : "+args.model_name+'_'str(args.test_episode))
+            print("Model : "+args.model_name+'_'+str(args.test_episode))
             self.model=torch.load('./model/hw4-2'+args.model_name+'_'+str(args.test_episode)+'.pkl')
             #you can load your model here
             #print('loading trained model')
@@ -43,11 +66,13 @@ class Agent_DQN(Agent):
             self.replay_buffer=collections.deque()
             self.replay_buffer_capacity=args.capacity
             self.traincurve=[]
-            self.eps=args.eps
+            self.eps_start=args.eps_start
             self.eps_decay=args.eps_decay
-            self.target_update=args.target_update
-            
-            self.model=nn.ModuleList([nn.Sequential(nn.Conv2d(1,3,kernel_size=4,stride=2,padding=0), # (1,3,39,39)
+            self.eps_end=args.eps_end
+            self.target_update=args.target_update        
+            self.train_method=args.train_method #Must be "Epsilon" or "Boltzmann"
+            """
+            self.policy_model=nn.ModuleList([nn.Sequential(nn.Conv2d(1,3,kernel_size=4,stride=2,padding=0), # (1,3,39,39)
                                                           nn.ReLU(),
                                                           nn.Conv2d(3,3,kernel_size=4,stride=2,padding=0), # (1,3,18,18)
                                                           ),
@@ -61,7 +86,10 @@ class Agent_DQN(Agent):
                                                           nn.Sigmoid(),
                                                           ),
                                             ]).cuda()
-
+            self.target_model=self.policy_model
+            """
+            self.policy_model=DQN()
+            self.traget_model=DQN()
         ##################
         # YOUR CODE HERE #
         ##################
@@ -87,8 +115,14 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-        pass
-
+        for episode in range(self.episode):
+            for times in range(self.target_update):
+                
+        #pass
+    def update_epsilon(eps_start,eps_end,eps_decay):
+        if eps_start>eps_end:
+            eps_start=eps_start-eps_decay
+        return eps_start
 
     def make_action(self, observation, test=True):
         """
@@ -105,6 +139,24 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-        return self.env.get_random_action()
+        #1:NO-OP 2:Left 3:Right
+        q_value=torch.Tensor(self.policy_model)
+        if self.train_method=="Epsilon":
+            c=np.random.uniform()
+            if c>self.eps_start:
+                action=torch.argmax(q_value).item()
+            else:
+                action=np.random.randint(1,4)
+        elif self.tain_method=="Boltzmann":
+            probability=F.Softmax(q_value,dim=1)
+            cumulated=0
+            threshold=np.random.uniform()
+            for i in range(probability.shape[1]):
+                cumulated+=probability[0,i]
+                if cumulated>threshold:
+                    action=i
+                    break
+        return action
+        #self.env.get_random_action()
 
        
