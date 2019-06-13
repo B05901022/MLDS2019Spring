ce2d81f2b05f5fe09d5647a1cbd6cbd43dd39119
Serenade34 <43457610+Serenade34@users.noreply.github.com>
2019-06-14 01:26:32 +0800

    Add files via upload

--- hw4/argument.py
+++ hw4/argument.py
@@ -7,29 +7,34 @@ def add_arguments(parser):
         parser.add_argument('--batch_size', type=int, default=32, help='batch size for training')
         parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate for training')
     '''
-    parser.add_argument('--load_model', type=bool, default=True,    help='Load pre-trained model')
-    parser.add_argument('--model_name', type=str,  default='PG_CV3',help='Name of your model')
-    parser.add_argument('--optim',      type=str,  default='Adam',  help='Optimizer type')
-    parser.add_argument('--lr',         type=float,default=0.0001,  help='Learning rate')
-    parser.add_argument('--gamma',      type=float,default=0.99,    help='Discout factor')
-    parser.add_argument('--batchsize',  type=int,  default=1,       help='Batch size')
-    parser.add_argument('--episode',    type=int,  default=10000,   help='Episodes')
+    parser.add_argument('--load_model', type=bool, default=False,    help='Load pre-trained model')
     parser.add_argument('--ppo',        type=bool, default=False,   help='Proximal policy optimization')
     parser.add_argument('--vanilla_pg', type=bool, default=False,   help='If use vanilla policy gradient')
-    parser.add_argument('--test_episode',type=int, default=1600,    help='Testing/Retraining episode')
-
-    parser.add_argument('--dqn_model_name',     type=str,  default='DQN_CV1',help='Name of your model')
-    parser.add_argument('--dqn_test_episode',   type=int,  default=1000,     help='Testing/Loading on which episode')
-    parser.add_argument('--dqn_episode',        type=int,  default=10000,    help='Episodes')
-    parser.add_argument('--dqn_batchsize',      type=int,  default=32,       help='Batch size')
-    parser.add_argument('--dqn_capacity',       type=int,  default=10000,    help='How big is the buffer')
-    parser.add_argument('--dqn_eps_start',      type=int,  default=200,      help='Episilon greedy start')
-    parser.add_argument('--dqn_eps_decay',      type=int,  default=500,      help='Episilon greedy decay')
-    parser.add_argument('--dqn_eps_end',        type=int,  default=1500,     help='Episilon greedy end')
-    parser.add_argument('--dqn_current_update', type=int,  default=4,        help='Current update step')
-    parser.add_argument('--dqn_target_update',  type=int,  default=1000,     help='Target update step')
-    parser.add_argument('--dqn_train_method',   type=str,  default='Epsilon',help='Must be \'Epsilon\' or \'Boltzmann\'')
-    parser.add_argument('--dqn_optim',          type=str,  default='Adam',   help='Currently supporting RMSprop, Adam, SGD')
-    parser.add_argument('--dqn_lr',             type=float,default=1.5e-4,   help='Learning rate')
-
+    
+    
+    parser.add_argument('--dqn_model_name', type=str,  default='DQN_CV1',help='Name of your model')
+    parser.add_argument('--dqn_optim',      type=str,  default='Adam',  help='Optimizer type (SGD,Adam,RMSprop)')
+    parser.add_argument('--dqn_train_method', type=str, default="Epsilon",   help='Choose Epsilon or Boltzmann')
+    parser.add_argument('--model_name', type=str,  default='PG_CV3',help='Name of your model')
+    parser.add_argument('--optim',      type=str,  default='Adam',  help='Optimizer type')
+    parser.add_argument('--dqn_lr',         type=float,default=0.0001,  help='Learning rate')
+    parser.add_argument('--dqn_eps_start',      type=float,default=1,    help='Epsilon start value')
+    parser.add_argument('--dqn_eps_decay',      type=float,default=0.0001,    help='Epsilon decay value')
+    parser.add_argument('--dqn_eps_end',      type=float,default=0.025,    help='Epsilon minimum value')
+    parser.add_argument('--lr',         type=float,default=0.0001,  help='Learning rate')
+    parser.add_argument('--gamma',      type=float,default=0.99,    help='Discout factor')
+    parser.add_argument('--dqn_episode',    type=int,  default=10000,   help='Episodes')
+    parser.add_argument('--dqn_capacity',    type=int,  default=10000,   help='How many samples can the replay buffer keep?')
+    parser.add_argument('--dqn_target_update',    type=int,  default=10000,   help='How many times can the policy model update after the target model updates?')
+    parser.add_argument('--dqn_sample_frequency',    type=int,  default=4,   help='Sample frequency')
+    parser.add_argument('--batchsize',  type=int,  default=1,       help='Batch size')
+    parser.add_argument('--episode',    type=int,  default=10000,   help='Episodes')
+    parser.add_argument('--test_episode',    type=int,  default=10000,   help='Eest episode')
+    parser.add_argument('--dqn_batchsize',  type=int,  default=64,       help='Batch size')
+    parser.add_argument('--dqn_episode',type=int, default=1600,    help='Testing/Retraining episode')
+    
+    
+    
+    
+    
     return parser
